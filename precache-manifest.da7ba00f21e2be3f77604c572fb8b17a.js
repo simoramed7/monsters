39a131ee36b35a1e0c76633350214ff30cd27712
unknown <sr-_-7@hotmail.com>
2020-06-01 23:58:38 +0100

    Updates

---
+++ precache-manifest.da7ba00f21e2be3f77604c572fb8b17a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d822ca5da41816a1bd3d2a0432578893",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5c99644cf48bec4101e8",
+    "url": "/monsters/static/css/main.eb9cd096.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/monsters/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c99644cf48bec4101e8",
+    "url": "/monsters/static/js/main.3f98ca98.chunk.js"
+  },
+  {
+    "revision": "5520c3e6235208f1eea1",
+    "url": "/monsters/static/js/runtime-main.59ed3f86.js"
+  }
+]);
